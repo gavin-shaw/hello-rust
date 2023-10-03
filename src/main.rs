ec156ef7fdce2cadf2353c3d043c3e039ff1e59c
Gavin Shaw <gavin@gnarus.io>
2023-10-03 18:41:04 +0200

    fix: missing user agent causing error

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use serde::Deserialize;
+use reqwest::header::USER_AGENT;
 use reqwest::Error;
+use serde::Deserialize;
 
+#[allow(dead_code)]
 #[derive(Deserialize, Debug)]
 struct User {
     login: String,
@@ -9,11 +11,19 @@ struct User {
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let request_url = format!("https://api.github.com/repos/{owner}/{repo}/stargazers",
-                              owner = "rust-lang",
-                              repo = "rust");
-    println!("{}", request_url);
-    let response = reqwest::get(&request_url).await?;
+    let request_url = format!(
+        "https://api.github.com/repos/{owner}/{repo}/stargazers",
+        owner = "rust-lang",
+        repo = "rust"
+    );
+
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get(request_url)
+        .header(USER_AGENT, "Hello Rust")
+        .send()
+        .await?;
 
     let users: Vec<User> = response.json().await?;
     println!("{:?}", users);
